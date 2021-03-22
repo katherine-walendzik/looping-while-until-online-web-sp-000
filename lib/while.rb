4ab616efb65155fd9bedc3a369ff4afe177a772b
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-22 00:10:58 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
-  counter = 0
-  while counter < 10
+  levitation_force = 0
+  while levitation_force < 10
     puts "Wingardium Leviosa"
-    counter += 1
+    levitation_force += 1
   end
 end
