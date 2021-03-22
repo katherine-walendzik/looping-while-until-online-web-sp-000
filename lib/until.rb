9102727d5ee136865f3febe3f6ea03e136e3162d
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-22 00:07:36 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,7 @@
 def using_until
-  levitation_force = 6
-
-  until levitation_force == 10
+  force = 0
+  until force == 10
     puts "Wingardium Leviosa"
-    levitation_force += 1
+    force += 1
   end
 end
--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,7 @@
 def using_while
-  levitation_force = 6
-	
-  while levitation_force < 10
+  force = 0
+  while force < 11
     puts "Wingardium Leviosa"
-    levitation_force += 1
+    force += 1
   end
 end
